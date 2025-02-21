2d9a0c425de04f7363ac7bf69c092efda9a2e57d
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-02-21 11:06:20 -0400

    Implement  for CSV file handling and update file path in ArquivoTexto

--- lab6_ex.py
+++ lab6_ex.py
@@ -5,7 +5,7 @@ class ArquivoTexto(object):
 
     def extrair_conteudo(self):
         conteudo = list()
-        with open(f"/data/{self.arquivo}", "r") as arquivo:
+        with open(f"data/{self.arquivo}", "r") as arquivo:
             conteudo = arquivo.readlines()
         return [linha for linha in conteudo]
 
@@ -17,8 +17,39 @@ class ArquivoTexto(object):
 
 
 musica = ArquivoTexto("musica.txt")
-print(musica.conteudo)
+# print(musica.conteudo)
 
 
 # licao 2
 
+import sys
+
+sys.path.insert(0, "data")
+
+
+class ArquivoCSV(ArquivoTexto):
+
+    def __init__(self, arquivo: str):
+        super().__init__(arquivo=arquivo)
+        self.colunas = self.extrair_nome_colunas()
+
+    def extrair_nome_colunas(self):
+        primeira_linha = self.conteudo[0]
+        if (primeira_linha, list):
+            return primeira_linha
+        return primeira_linha.strip().split(",")
+
+    def extrair_coluna(self, indice_coluna: int):
+        indice_coluna -= 1
+        coluna = list()
+        for linha in self.conteudo[1:]:
+            coluna.append(linha.strip().split(",")[indice_coluna])
+        return coluna
+
+
+arquivo = ArquivoCSV("arquivo-csv.csv")
+print(arquivo.conteudo)
+print(arquivo.colunas)
+print(arquivo.extrair_coluna(2))
+print(arquivo.extrair_linha(2))
+print(arquivo.extrair_linha(3))
